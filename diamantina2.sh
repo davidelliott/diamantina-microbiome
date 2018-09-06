c5534a30ac3f4217c4f130d2663a34e7a0dcf63d
David Elliott <david@drelliott.net>
2018-09-06 08:15:45 +0100

    Adjust to use usearch 6.1 with inclusion of chimera checking

--- diamantina2.sh
+++ diamantina2.sh
@@ -32,6 +32,32 @@ validate_mapping_file.py -m map.txt -o map_check/ -p -b
 # split libraries
 multiple_split_libraries_fastq.py -i joined -o ./ --demultiplexing_method sampleid_by_file --include_input_dir_path --remove_filepath_in_name
 
+### new section here 5 September 2018 ####
+
+# chimera check using usearch 6.1 (see http://qiime.org/tutorials/chimera_checking.html)
+identify_chimeric_seqs.py -i /home/david/git/diamantina-microbiome/data/seqs.fna -m usearch61 -o usearch_checked_chimeras/ -r /home/david/miniconda3/envs/qiime1/lib/python2.7/site-packages/qiime_default_reference/gg_13_8_otus/rep_set/97_otus.fasta
+
+# filter chimeras
+filter_fasta.py -f /home/david/git/diamantina-microbiome/data/seqs.fna -o /home/david/git/diamantina-microbiome/data/seqs_chimeras_filtered.fna -s /home/david/git/diamantina-microbiome/usearch_checked_chimeras/chimeras.txt -n
+
+# pick otus, assign taxonomy, make otu table
+pick_open_reference_otus.py -i /home/david/git/diamantina-microbiome/data/seqs_chimeras_filtered.fna -o /home/david/git/diamantina-microbiome/data/usearch61_picked_otus -m usearch61 -a -O 3
+
+# copy the biom file which we will be using in downstream analyses and providing as supplementary data:
+cp data/usearch61_picked_otus/otu_table_mc2_w_tax_no_pynast_failures.biom data/otus/Elliott_Diamantina_Microbiome_Data_S1[otu-tax].biom
+
+## note
+# at wernerlab http://www.wernerlab.org/software/macqiime/citations it says "Another new chimera screening option is to use the UCHIME chimera detection during the OTU-picking step (pick_otus.py -m usearch61 ...), which means you never have to use identify_chimeric_seqs.py.". 
+# at QIIME documentation http://qiime.org/scripts/pick_otus.html it says "Chimera checking with usearch 6.X is implemented in identify_chimeric_seqs.py. Chimera checking should be done first with usearch 6.X, and the filtered resulting fasta file can then be clustered. ".
+
+# These 2 seem in contradiction. I have done it the way stated in QIIME docs for now but this should be checked by also doing it like this (without prior chimera check):
+### pick_open_reference_otus.py -i /home/david/git/diamantina-microbiome/data/seqs.fna -o /home/david/git/diamantina-microbiome/data/otus -m usearch61
+
+
+
+
+### end of new section - stuff below here was in the original version ###
+
 # pick OTUs, assign taxonomy, make otu table
 pick_open_reference_otus.py -i /home/david/git/diamantina-microbiome/data/seqs.fna -o /home/david/git/diamantina-microbiome/data/otus
 
